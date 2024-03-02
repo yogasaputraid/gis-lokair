5e0ea52043a02b0c80c8c7886a23dce87fb8d9c4
Yoga Saputra <gyo.yoga@gmail.com>
2024-03-02 19:08:39 +0700

    Update mapbox dan token

--- application/views/peta/js/mapHomeJs.php
+++ application/views/peta/js/mapHomeJs.php
@@ -32,11 +32,11 @@
 	// var layersBangunan = [];
 	// var layersCakupan = [];
 	// var layersKecamatan = [];
-	var Layer = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+	var Layer = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
 		maxZoom: 20,
-		id: 'mapbox.streets',
-		accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw'
+		id: 'mapbox/streets-v12',
+		accessToken: 'pk.eyJ1IjoieW9nYXNhcHV0cmEiLCJhIjoiY2w1Z3ZsNTg4MDNrYjNpbDJiZzB0MXMwbiJ9.5-Y1TBUidWasnMDJ_XReOw'
 	});
 	var sateliteMutant = L.gridLayer.googleMutant({
 		maxZoom: 20,
